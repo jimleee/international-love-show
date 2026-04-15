1c2391d802343d8578b287698d1caab60f121503
Jim Lee <jimlee1015@gmail.com>
2026-04-15 16:17:25 +0800

    feat(home): assemble full home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,28 @@
+import { Helmet } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
+import HeroCarousel from "@/components/home/HeroCarousel";
+import ManifestoSection from "@/components/home/ManifestoSection";
+import AboutSection from "@/components/home/AboutSection";
+import AdvantagesSection from "@/components/home/AdvantagesSection";
+import SixTeamsSection from "@/components/home/SixTeamsSection";
+import CourseSystemSection from "@/components/home/CourseSystemSection";
+import TeamShowcaseSection from "@/components/home/TeamShowcaseSection";
+
 export default function Home() {
-  return <div className="container mx-auto py-12">Home</div>;
+  const { t } = useTranslation();
+  return (
+    <>
+      <Helmet>
+        <title>{t("brand")}</title>
+        <meta name="description" content={t("home.manifesto.subtitle") as string} />
+      </Helmet>
+      <HeroCarousel />
+      <ManifestoSection />
+      <AboutSection />
+      <AdvantagesSection />
+      <SixTeamsSection />
+      <CourseSystemSection />
+      <TeamShowcaseSection />
+    </>
+  );
 }
